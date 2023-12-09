13de2531fb3d924b5027cc1dece8b06f7538e935
sirius1516 <gontarenco.s@gmail.com>
2023-12-09 22:31:03 +0100

    name and namespace

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from myapp.views import index, contacts, index_item
 
+# app_name = "myapp"
+
 urlpatterns = [
     path('hello/', index),
-    path('hello/<int:my_id>/', index_item),
+    path('hello/<int:my_id>/', index_item, name="detail"),
     path('contacts/', contacts),
 ]
