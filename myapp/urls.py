1868664ea03053903b4d11c57fb2fe9251967fe0
sirius1516 <gontarenco.s@gmail.com>
2024-01-31 14:42:02 +0100

    created update item function and template

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from myapp.views import index, index_item, add_item
+from myapp.views import index, index_item, add_item, update_item
 
 
 app_name = "myapp"
@@ -7,7 +7,8 @@ app_name = "myapp"
 urlpatterns = [
     path('', index),
     path('<int:my_id>/', index_item, name="detail"),
-    path('additem/', add_item, name="add_item")
+    path('additem/', add_item, name="add_item"),
+    path("update_item/<int:my_id>/", update_item, name="update_item"),
 ]
 
 
