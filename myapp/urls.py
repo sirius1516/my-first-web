4ef4ae6ef967c813dcaaa1a92e4977ca2f84b3d0
sirius1516 <gontarenco.s@gmail.com>
2024-02-08 17:06:39 +0100

    added delete.html template and function

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
-from myapp.views import index, index_item, add_item, update_item
+from myapp.views import index, index_item, add_item, update_item, delete_item
 
 
 app_name = "myapp"
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('<int:my_id>/', index_item, name="detail"),
     path('additem/', add_item, name="add_item"),
     path("update_item/<int:my_id>/", update_item, name="update_item"),
+    path("deleteitem/<int:my_id>", delete_item, name="delete_item"),
 ]
 
 
